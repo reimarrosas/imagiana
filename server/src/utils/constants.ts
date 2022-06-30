d15fd692d947d61b1cc70a69f9afef2d72a9a093
Reimar Rosas <reimarrosas0208@gmail.com>
2022-06-30 12:49:12 -0400

    Changed SESSION_URL to REDIS_URL

--- server/src/utils/constants.ts
+++ server/src/utils/constants.ts
@@ -3,7 +3,7 @@ export const port = process.env["PORT"] ?? 8080;
 export const sessionSecret = process.env["SESSION_SECRET"] ?? "keyboard cat";
 
 export const sessionStoreUrl =
-  process.env["SESSION_URL"] ?? "redis://localhost:6379";
+  process.env["REDIS_URL"] ?? "redis://localhost:6379";
 
 export const databaseUrl =
   process.env["DATABASE_URL"] ?? "postgres://reimar:reimar@localhost/imagiana";
